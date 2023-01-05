dd31a3bb9146bb46d20ef5d577acf6eecb749af6
sixdgt <c4crypt@gmail.com>
2023-01-05 16:02:58 +0545

    category & hospital model created and migrated

--- hospitalproject/hospitals/models.py
+++ hospitalproject/hospitals/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    category_code = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = "app_categories"
+
+class Hospital(models.Model):
+    full_name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    contact = models.IntegerField()
+    added_at = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        db_table = "app_hospitals"
