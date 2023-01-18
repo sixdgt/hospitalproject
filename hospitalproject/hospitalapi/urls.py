eec0287421793586d93cf3c09f9939f92835fb04
sixdgt <c4crypt@gmail.com>
2023-01-18 15:58:54 +0545

    hospital list & add from api

--- hospitalproject/hospitalapi/urls.py
+++ hospitalproject/hospitalapi/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import HospitalApiView
 
 urlpatterns = [
-    
+    path('hospital/', HospitalApiView.as_view())
 ]
--- hospitalproject/hospitalapi/views.py
+++ hospitalproject/hospitalapi/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from hospitalapi.serializers import HospitalSerializer, CategorySerializer
+from hospitals.models import Hospital, Category
+
 # Create your views here.
+def getSuccessResponse(data, code, msg):
+    response_data = {
+            'message': msg,
+            'status_code': code,
+            'data': data,
+            'error': [],
+            'pagination': [],
+        }
+    return response_data
+
+def getErrorResponse(error, code, msg):
+    response_data = {
+            'message': msg,
+            'status_code': code,
+            'data': [],
+            'error': error,
+            'pagination': [],
+        }
+    return response_data
+
 class HospitalApiView(APIView):
     def get(self, request):
-        pass
+        if request.method != "GET":
+            msg = 'Method now allow'
+            return Response(getErrorResponse(msg, 405, msg))
+        data = Hospital.objects.all()
+        serializer = HospitalSerializer(data, many=True)
+        msg = 'Hospital List'
+        return Response(getSuccessResponse(serializer.data, 200, msg), status=status.HTTP_200_OK)
+
     def post(self, request):
-        pass
+        if request.method == "POST":
+            req_data = {
+                'full_name': request.POST.get('full_name'),
+                'short_name': request.POST.get('short_name'),
+                'category': 1,
+                'contact': request.POST.get('contact'),
+                'address': request.POST.get('address'),
+            }
+            serializer = HospitalSerializer(data=req_data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(getSuccessResponse(serializer.data, 201, 'Added success'))
+            else:
+                return Response(getErrorResponse(serializer.errors, 400, 'Something went wrong'))
+        msg = 'Method now allow'
+        return Response(getErrorResponse(msg, 405, msg))
